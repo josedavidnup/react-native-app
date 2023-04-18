964b1ddf851c779bb654bdc4064d185d9502f1ec
josedavidnup <josedavidnup@gmail.com>
2023-04-17 21:21:36 -0400

    new boilerplate with just js

--- src/utils/getColorPokemonByType.ts
+++
@@ -1,6 +0,0 @@
-import { PokemonTypeColor } from './constants';
-
-export const getColorPokemonByType = (types: string[]): string[] =>
-  types.map(
-    (type) => PokemonTypeColor[type as keyof typeof PokemonTypeColor] //this is how to type it
-  );
