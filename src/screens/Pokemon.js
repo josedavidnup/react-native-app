1b86e658a4b8650f7a63e0caf334e8b0d5e34d90
josedavidnup <josedavidnup@gmail.com>
2023-04-20 17:02:48 -0400

    navigation style

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView } from 'react-native';
 import Header from '../components/Pokemon/Header';
 import Type from '../components/Pokemon/Type';
 import Stats from '../components/Pokemon/Stats';
+import { FontAwesome } from '@expo/vector-icons';
 
-const Pokemon = ({ route }) => {
-  const { pokemon } = route.params;
+const Pokemon = ({ route: { params }, navigation }) => {
+  const { pokemon } = params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <FontAwesome
+          name='arrow-left'
+          color={'#fff'}
+          size={20}
+          // style={{ marginLeft: 15 }}
+          onPress={() => navigation.goBack()}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   if (!pokemon) return null;
   return (
